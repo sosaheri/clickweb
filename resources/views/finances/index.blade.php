9e5e2c02faeb21c8bc5d0b6123473150915948b9
sosaheri <sosaheriberto2001@gmail.com>
2021-04-05 17:04:29 -0400

    feat: add payments policy.

--- resources/views/finances/index.blade.php
+++ resources/views/finances/index.blade.php
@@ -6,7 +6,7 @@
     <div class="header bg-gradient-primary pb-8 pt-5 pt-md-8">
     </div>
     <div class="container-fluid mt--9">
-        
+
         <div class="row">
             @isset($showFeeTerms))
                 @include('finances.feeterms')
@@ -17,17 +17,17 @@
         </div>
 
         <br />
-        
+
         <!-- Info cards -->
         @isset($cards)
-            @include('partials.infoboxes.index') 
+            @include('partials.infoboxes.index')
         @endisset
 
 
         <br />
         <div class="row">
               <div class="col">
-              
+
             </div>
             <div class="col">
                 <!-- Order Card -->
@@ -35,13 +35,37 @@
                <h2>Planes</h2>
             </div>
               <div class="col">
-           
+
             </div>
         </div>
        <div class="row">
-           
-           Plan Estandar:  15$ mensual, Si tu negocio no tiene más 10 pedido siguiente mes es GRATIS.
+
+           <div>
+            Plan Estandar:  Si tu negocio no tiene más 15 pedido siguiente mes es GRATIS<br>
+           El costo mensual del servicio es de 20$<br><br>
+
+
+           </div>
+
        </div>
+
+       <div>
+                      Formas de pago:<br>
+          <ul>
+               <li>Dash</li>
+               <li>Paypal</li>
+               <li>Efectivo</li>
+               <li>Pago efectivo</li>
+           </ul>
+       </div>
+
+       <div>
+
+           Pago efectivo hay enviar correo electrónico a <a href="mailto:pagoefectivo@clicweb.net">pagoefectivo@clicweb.net</a> (negocio, código
+            postal, ciudad) y después pagar hay enviar <a href="mailto:albaran@clicweb.net">albaran@clicweb.net</a> justificante.
+
+       </div>
+
 
 
     </div>
